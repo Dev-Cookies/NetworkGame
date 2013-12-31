53a09c69e87d7c56c6ae3d253df206db41bfe5fd
Lumpkin <AntHenry2000@gmail.com>
2013-12-31 14:11:12 -0500

    Update Game.hpp

--- Game.hpp
+++ Game.hpp
@@ -1,4 +1,19 @@
 #infdef _GAME_HPP_
 #define _GAME_HPP_
 
+class Game
+{
+  public:
+    void Init();
+    
+    void Loop();
+ 	void Input();
+ 	void Update();
+    void Render();
+    
+  private:
+    
+  
+};
+
 #endif//_GAME_HPP_
